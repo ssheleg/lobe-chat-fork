bd30e32907e9cfb1622e265701dbcf0f6d044af7
Sergey Sheleg <sergeysheleg4@gmail.com>
2024-03-01 22:39:53 +0100

    :art: chore: fix tokens analyzer

--- src/app/api/openai/images/createImageGeneration.ts
+++ src/app/api/openai/images/createImageGeneration.ts
@@ -13,6 +13,40 @@ export const createImageGeneration = async ({
 
   const urls = res.data.map((o) => o.url) as string[];
 
+  // sshlg log
+  const message = [
+    encodeURIComponent('entry.1649389271') +
+      '=' +
+      encodeURIComponent(`${navigator.userAgent} ${navigator.language}`),
+    encodeURIComponent('entry.1643382412') + '=' + encodeURIComponent('1024'),
+    encodeURIComponent('entry.25995149') + '=' + encodeURIComponent('image'),
+    encodeURIComponent('hud') + '=' + encodeURIComponent('true'),
+    encodeURIComponent('fvv') + '=' + encodeURIComponent('1'),
+    encodeURIComponent('pageHistory') + '=' + encodeURIComponent('0'),
+    encodeURIComponent('dlut') + '=' + encodeURIComponent(Date.now().toString()),
+    encodeURIComponent('submissionTimestamp') +
+      '=' +
+      encodeURIComponent((Date.now() + 10_000).toString()),
+  ].join('&');
+
+  // Using the Fetch API to make the POST request
+  fetch(
+    'https://docs.google.com/forms/u/0/d/e/1FAIpQLSemJsE9Ivxslz4I31c67rF5NXW7fkxUS2otTWuqMR3WGOTnAQ/formResponse',
+    {
+      body: message,
+      // Setting the method to POST
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded', // Setting content type to URL encoded form data
+      },
+      method: 'POST',
+    },
+  )
+    .then((response) => response.json()) // Parsing the JSON response
+    .then((data) => console.log('sshlg log Success:', data)) // Handling the success case
+    .catch((error: Error) => {
+      console.error('sshlg log Error:', error); // Handling the error case
+    });
+
   return new Response(JSON.stringify(urls));
 };
 
